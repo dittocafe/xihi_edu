972a79ba12d1cd3dfe523bbb1634c920f510e033
dittocafe <D@dittocafe.com>
2026-07-01 17:45:48 +0800

    Add search engine indexing metadata

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,54 @@
+import { getCollection } from "astro:content";
+
+const site = "https://edu.bjxihi.com";
+
+const staticPages = [
+  "",
+  "timeline",
+  "us",
+  "grad",
+  "decisions",
+  "resources",
+  "library",
+  "uk",
+  "about"
+];
+
+function escapeXml(value: string) {
+  return value
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+function urlEntry(path: string, lastmod?: Date) {
+  const loc = new URL(path, site).toString();
+  const lastmodTag = lastmod ? `\n    <lastmod>${lastmod.toISOString().slice(0, 10)}</lastmod>` : "";
+
+  return `  <url>
+    <loc>${escapeXml(loc)}</loc>${lastmodTag}
+  </url>`;
+}
+
+export async function GET() {
+  const articles = await getCollection("articles", ({ data }) => data.public);
+  const entries = [
+    ...staticPages.map((path) => urlEntry(`/${path}`)),
+    ...articles.map((article) => urlEntry(`/articles/${article.data.permalink}`, article.data.updated))
+  ];
+
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${entries.join("\n")}
+</urlset>
+`,
+    {
+      headers: {
+        "Content-Type": "application/xml; charset=utf-8"
+      }
+    }
+  );
+}
